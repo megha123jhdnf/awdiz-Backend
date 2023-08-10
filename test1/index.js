447b17f811fdf5592326f15e2512a8002dcfda4c
megha123jhdnf <132134321+megha123jhdnf@users.noreply.github.com>
2023-08-10 20:30:49 +0530

    update

--- test1/index.js
+++ test1/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { Login, Register } from "./controllers/User.controller.js";
+import mongoose from "mongoose";
 
 const app = express();
 
@@ -13,12 +14,16 @@ app.get("/login", Login )
 //     res.send("Login")
 
 // })
-
 app.get("/register", Register)
 
-// app.get("/register", function(req,res){
+// app.get("/register", function (req, res) {
 //     res.send("Register")
 // })
+
+mongoose.connect("mongodb+srv://meghali:megha123@cluster0.4oyog0q.mongodb.net/awdiz-backend").then(()=>{
+    console.log("connected to db.")
+}).catch((error)=>{
+    console.log("Error while connecting mongodb",error)})
 
 app.listen(8000,()=>{
     console.log("Server listening on port 8000");
