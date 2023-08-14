8d0379bc53d8f9ae13c3c27e93baa39a63b290ef
megha123jhdnf <132134321+megha123jhdnf@users.noreply.github.com>
2023-08-14 18:27:58 +0530

    update

--- test3/index.js
+++ test3/index.js
@@ -7,52 +7,47 @@ const app = express();
 dotenv.config();
 app.use(express.json());
 
-// CRUD - CREATE READ  UPDATE   DELETE
-// METHODS  POST GET  PATCH/PUT DELETE
+// CRUD -  CREATE  READ  UPDATE     DELETE
+// METHOD- POST    GET   PATCH/PUT   DELETE
 
-app.get("/", function (req, res) {
-    res.send("Working...")
+app.get("/",function(req,res){
+    res.send("working..")
 })
+app.post('/login', function(req,res){
+    res.send("Hello from login function")
 
-app.post('/login', function (req, res) {
-    res.send("Hello from Login function..")
 })
-
-app.post('/register', async function (req, res) {
-    console.log(req.body, "req.body")
-    const { name, surname, age, email, number, password, confirmPassword } = req.body;
-    if (!name) return res.send("Name is missing..");
+app.post('/register', async function(req,res){
+    console.log(req.body,"req.body")
+    const {name,surname,age,number,email,password,confirmpassword} = req.body;
+    if(!name) return res.send("Name is missing");
     if (!surname) return res.send("Surname is missing..")
     if (!age) return res.send("Age is missing..")
     if (!email) return res.send("Email is required.")
     if (!number) return res.send("Number is required");
     if (!password) return res.send("Password is required");
-    if (!confirmPassword) return res.send("Confirm password is required!")
-    if (password !== confirmPassword) return res.send("Password and Confirm password not matched.")
-
-    console.log(typeof (age), typeof (number), "data types")
+    if (!confirmpassword) return res.send("Confirm password is required!")
+    if (password !== confirmpassword) return res.send("Password and Confirm password not matched.")
 
     const user = new User({
-        name: name,
-        surname: surname,
-        age: parseInt(age),
+        name:name,
+        surname:surname,
+        age:age,
+        number,
         email,
-        number: parseInt(number),
         password
     })
-
     // console.log(user,"user")
 
     await user.save()
-
-    res.send("Registeration Done..")
+    res.send("registeration done")
 
 })
 
-mongoose.connect(process.env.MONGO_URL).then(() => {
-    console.log("Connected to DB..")
+mongoose.connect(process.env.MONGO_URL).then(()=>{
+    console.log("Connected to db")
 })
 
-app.listen(8000, () => {
-    console.log("Listening on port 8000");
+app.listen(8000,()=>{
+    console.log("listening on port 8000")
 })
